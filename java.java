b71ea657885d75300f58af7d0d0e087b6546257c
tanmayvidhate16 <tanmayvidhate16@gmail.com>
2024-11-13 17:10:38 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 public class java {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!")
     }
 }
